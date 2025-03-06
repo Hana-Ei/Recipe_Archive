1e891aee2bb70bf925bf18e7ea81850ae9b2da6e
Hana-Ei <tldbWkd0830@gmail.com>
2025-03-06 09:54:41 +0900

    first Project File 2025/03/06 09:24

--- src/main/java/com/ventogumi/recipe/RecipeArchiveApplication.java
+++ src/main/java/com/ventogumi/recipe/RecipeArchiveApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecipeArchiveApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RecipeArchiveApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeArchiveApplication.class, args);
+	}
 
 }
