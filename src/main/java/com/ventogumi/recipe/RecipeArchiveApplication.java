149407218b2966e4822b2438b57ba8b37e485fde
Hana-Ei <tldbWkd0830@gmail.com>
2025-03-06 09:24:23 +0900

    first Project File 2025/03/06 09:24

---
+++ src/main/java/com/ventogumi/recipe/RecipeArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.ventogumi.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeArchiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeArchiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ventogumi/recipe/RecipeArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventogumi.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeArchiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
