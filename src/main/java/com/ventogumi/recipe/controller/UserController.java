abc1523d20ad2e4fc9eec1b9e9ff2c441387032e
kang <qjarkd705766@gmail.com>
2025-03-11 23:38:21 +0900

    1. 로그인 html에서 회원가입 링크들어가도록 설정, 2. 회원가입 시 비밀번호 확인, 이메일 인증 뺌, 소셜 로그인 기능 뺌 3. 회원가입 시 오류 - null 허용하도록 스키마 설정
    아마 수영님이 해결해주실 듯

---
+++ src/main/java/com/ventogumi/recipe/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.ventogumi.recipe.controller;
+
+import com.ventogumi.recipe.model.user.User;
+import com.ventogumi.recipe.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    //회원가입 페이지 이동
+    @GetMapping(path = "users/register")
+    public String register(Model model) {
+
+        model.addAttribute("user", new User());
+        return "users/register";
+
+    }
+
+    // 회원가입
+    @PostMapping("users/register")
+    public String registerUser(@ModelAttribute User user) {
+        log.info("user: {}", user);
+        userService.saveUser(user);
+        return "redirect:/";
+    }
+
+    // 로그인 페이지 이동
+    @GetMapping("users/login")
+    public String login(Model model) {
+        model.addAttribute("user", new User());
+        return "users/login";
+    }
+}
--- src/main/java/com/ventogumi/recipe/repository/UserRepository.java
+++ src/main/java/com/ventogumi/recipe/repository/UserRepository.java
@@ -1,15 +1,8 @@
-//package com.ventogumi.recipe.repository;
-//
-//import java.util.Optional;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import com.example.user.model.User;
-//
-//@Repository
-//public interface UserRepository extends JpaRepository<User, Long> {
-//
-//	Optional<User> findByUsername(String username);
-//
-//}
+package com.ventogumi.recipe.repository;
+
+import com.ventogumi.recipe.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
